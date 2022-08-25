8eb783f523e30c52c99fec2c587e1267548683ce
Mittensdlara <pooraghaeedelara@gmail.com>
2022-08-25 16:25:19 +0430

    update

--- src/Translation.js
+++ src/Translation.js
@@ -4,20 +4,87 @@ import "./Translation.css"
 import Results from "./Results"
 
 export default function Translation(props) {
+ let [text, setText] = useState(props.defaultText);
  let [loaded, setLoaded] = useState(false)
+ let [results, setResults] = useState(null);
+ 
+function handleResponse(response) {
+    console.log(response.data);
+    setResults(response.data);
+}
+
+
+ function handleTranslate() {
+    
+const encodedParams = new URLSearchParams();
+encodedParams.append("q", "Hello, world!");
+encodedParams.append("target", "es");
+encodedParams.append("source", "en");
+
+const options = {
+  method: 'POST',
+  url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
+  headers: {
+    'content-type': 'application/x-www-form-urlencoded',
+    'Accept-Encoding': 'application/gzip',
+    'X-RapidAPI-Key': '2583a75a4dmshb34f90d0e9e5eeap173ba6jsn7a5f43d6c965',
+    'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+  },
+  data: encodedParams
+};
+
+axios.get(options).then(handleResponse)
+ }
+ 
+ 
+ function handleLanguages() {
+
+    const options = {
+        method: 'GET',
+        url: 'https://google-translate1.p.rapidapi.com/language/translate/v2/languages',
+        headers: {
+          'Accept-Encoding': 'application/gzip',
+          'X-RapidAPI-Key': '2583a75a4dmshb34f90d0e9e5eeap173ba6jsn7a5f43d6c965',
+          'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+        }
+      };
+      
+      axios.get(options).then(handleTranslate)
+  }
+
+
+function translate() {
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("q", `${text}`);
+
+const options = {
+  method: 'POST',
+  url: 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect',
+  headers: {
+    'content-type': 'application/x-www-form-urlencoded',
+    'Accept-Encoding': 'application/gzip',
+    'X-RapidAPI-Key': '2583a75a4dmshb34f90d0e9e5eeap173ba6jsn7a5f43d6c965',
+    'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+  },
+  data: encodedParams
+};
+axios.get(options).then(handleLanguages)
+
+}
+
 
  function handleSubmit(event) {
     event.preventDefault();
-    search();
+    translate();
   }
-  
+
  function handleTextChange(event) {
-    setKeyword(event.target.value);
+    setText(event.target.value);
   }
 
  function load() {
     setLoaded(true);
-    search();
+    translate();
   }
 
 
@@ -36,7 +103,7 @@ export default function Translation(props) {
        <div className="col-6">
         <p className="text-center">
         <div class="form-floating">
-             <textarea class="form-control" onChange={handleTextChange} placeholder="Text" id="floatingTextarea2"></textarea>
+             <textarea class="form-control" onChange={handleTextChange}  defaultValue={props.defaultText} placeholder="Text" id="floatingTextarea2"></textarea>
             <label for="floatingTextarea2">Translation</label>
        </div>
         </p>
@@ -44,7 +111,7 @@ export default function Translation(props) {
         <form className="Form">
             <input type="submit" value="Translate" onSubmit={handleSubmit} />
         </form>
-        <Results />
+        <Results results={results} />
        </div>
       </div>
     </div>
